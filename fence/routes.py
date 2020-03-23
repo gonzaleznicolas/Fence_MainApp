b9daa43d86335544bc894b6f0490126050fa0e8d
Nicolas Gonzalez <nicolas.g96@gmail.com>
2020-03-23 11:47:33 -0400

    visual improvements

--- fence/routes.py
+++ fence/routes.py
@@ -9,28 +9,28 @@ from flask_login import login_user, current_user, logout_user, login_required
 posts = [
     {
         "id": "0",
-        "author": "Joe",
-        "title": "this is a title",
-        "content": "Wow guys Im fencing this is so cool",
+        "author": "Joe Jackson",
+        "title": "What is software?",
+        "content": "Soeone explain it to me...",
         "comments": [
-            {"id": "1", "level": 0, "user": "jj", "content": "Ah"},
-            {"id": "2", "level": 1, "user": "rawr", "content": "What"},
-            {"id": "3", "level": 2, "user": "Hi", "content": "Ok"},
-            {"id": "4", "level": 2, "user": "Hi", "content": "poo"}
+            {"id": "1", "level": 0, "user": "Jack", "content": "Ah"},
+            {"id": "2", "level": 1, "user": "Charlie", "content": "What"},
+            {"id": "3", "level": 2, "user": "Alice", "content": "Ok"},
+            {"id": "4", "level": 2, "user": "Bob", "content": "lol"}
         ]
     },
     {
         "id": "1",
-        "author": "Steve",
-        "title": "this is a title 2",
-        "content": "K",
+        "author": "Steve Terry",
+        "title": "What is fence",
+        "content": "What is this website?",
         "comments": []
     },
     {
         "id": "2",
-        "author": "Mike",
-        "title": "this is a title 3",
-        "content": "SIKE MY GUYYYYYYYYYYYYY...",
+        "author": "Mike Lund",
+        "title": "I like software",
+        "content": "Do you all agree?",
         "comments": [
             {"id": "1", "level": 0, "user": "Jared", "content": "love it!"},
             {"id": "3", "level": 1, "user": "keemstar", "content": "love you!"},
