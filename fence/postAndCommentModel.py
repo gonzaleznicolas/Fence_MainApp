e0d02bfe1b4dc9bd6f2e1584b8e4c3cd94c3d14e
Nicolas Gonzalez <nicolas.g96@gmail.com>
2020-03-23 22:33:35 -0400

    wip

--- fence/postAndCommentModel.py
+++ fence/postAndCommentModel.py
@@ -3,17 +3,44 @@ from datetime import datetime
 from fence import db
 from fence.userModel import User
 
-fake_posts_db = {}
+fake_posts_db = { 
+	12345: 
+	{
+		"id": 12345,
+		"author_id": 1,
+		"time": datetime.now(),
+		"title": "This is a test title",
+		"content": "What should I make the content of this post?"
+	}
+}
 
-fake_comments_db = {}
+fake_comments_db = {
+	12345: [
+		{
+			"author_id": 1,
+			"time": datetime.now(),
+			"content": "What a stupid title..."
+		}
+	]
+}
 
+# expected to store the post with the passed in title, content, and author_id
 def newPost(title, content, author_id):
 	post_id = uuid.uuid1().int
 	time = datetime.now()
 
 	# replace this with writing an event to the event database
-	fake_posts_db[post_id] = {"id": post_id, "author_id": f"{author_id}", "title": title, "content": content}
+	fake_posts_db[post_id] = {"id": post_id, "author_id": author_id, "time": time, "title": title, "content": content}
 
+
+# input: integer (id of post to get)
+# expected output: dictionary with the following format
+# {
+# 	"id": <int>
+# 	"author_id": <int>
+# 	"title": <string>
+# 	"content": <string>
+# }
 def getPost(post_id):
 	post = fake_posts_db[post_id] # replace this with getting post from microservice
 
@@ -22,9 +49,8 @@ def getPost(post_id):
 	return post
 	
 
-def getCommentsForPost(post_id):
-	return fake_comments_db.get(post_id, [])
-
+# input: integer (number of posts to get)
+# expected output: an array of post dictionaries, of at most size numberOfPosts
 def getMostRecentPosts(numberOfPosts):
 	posts = fake_posts_db # replace this with getting post from microservice
 
@@ -36,4 +62,10 @@ def getMostRecentPosts(numberOfPosts):
 
 	print(fake_posts_db)
 	return posts.values()
+
+
+# input: id of a post
+# output: an array of comments in the following format:
+def getCommentsForPost(post_id):
+	return fake_comments_db.get(post_id, [])
 
