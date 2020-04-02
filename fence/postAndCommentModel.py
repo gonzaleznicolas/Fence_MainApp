6bf03d17351b8fff853a71a1f6497fffa0dbedf9
Nicolas Gonzalez <nicolas.g96@gmail.com>
2020-04-02 15:02:46 -0600

    fix datetime issue

--- fence/postAndCommentModel.py
+++ fence/postAndCommentModel.py
@@ -30,6 +30,7 @@ def commentOnPost(post_id, author_id, content):
 
 
 def newPost(title, content, author_id):
+	print(f"time we are writing in is {datetime.now()}")
 	# write new event to the event table
 	event = Event(event_name='new_post', title=title, content=content, author_id=author_id, time=datetime.now())
 	db.session.add(event)
@@ -39,7 +40,8 @@ def newPost(title, content, author_id):
 def getPost(post_id):
 	response = requests.get(f"{microserviceURL}/post/get/{post_id}")
 	post = response.json()
-
+	# turn date string to python datetime
+	post['time'] = datetime.strptime(post['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
 	# load username
 	usr = User.query.filter_by(id=post['author_id']).first()
 	post['author'] = usr.username if usr is not None else None
@@ -51,6 +53,8 @@ def getMostRecentPosts(numberOfPosts):
 	posts = response.json()
 	# load the username for each post in posts
 	for post in posts:
+		# turn date string to python datetime
+		post['time'] = datetime.strptime(post['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
 		# load username
 		usr = User.query.filter_by(id=post['author_id']).first()
 		post['author'] = usr.username if usr is not None else None
@@ -63,6 +67,8 @@ def getAllPosts():
 	posts = response.json()
 	# load the username for each post in posts
 	for post in posts:
+		# turn date string to python datetime
+		post['time'] = datetime.strptime(post['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
 		# load username
 		usr = User.query.filter_by(id=post['author_id']).first()
 		post['author'] = usr.username if usr is not None else None
@@ -76,12 +82,18 @@ def getCommentsForPost(post_id):
 
 	# set author for 3 levels of nested comments
 	for comment0 in comments:
+		# turn date string to python datetime
+		comment0['time'] = datetime.strptime(comment0['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
 		usr = User.query.filter_by(id=comment0['author_id']).first()
 		comment0['author'] = usr.username if usr is not None else None
 		for comment1 in comment0["comments"]:
+			# turn date string to python datetime
+			comment1['time'] = datetime.strptime(comment1['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
 			usr = User.query.filter_by(id=comment1['author_id']).first()
 			comment1['author'] = usr.username if usr is not None else None
 			for comment2 in comment1["comments"]:
+				# turn date string to python datetime
+				comment2['time'] = datetime.strptime(comment2['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
 				usr = User.query.filter_by(id=comment2['author_id']).first()
 				comment2['author'] = usr.username if usr is not None else None
 
